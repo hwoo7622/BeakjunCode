c21843bf9858044838a18d8f493c54dc4399c3b3
rarara <155744452+hwoo7622@users.noreply.github.com>
2025-04-05 18:56:22 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 12356 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val A = readLine()!!.toInt()
+    if(A>=90){
+        print("A")
+    }
+    else if(A>=80){
+        print("B")
+    }
+    else if(A>=70){
+        print("C")
+    }
+    else if(A>=60){
+        print("D")
+    }
+    else{
+        print("F")
+    }
+
+}
+
