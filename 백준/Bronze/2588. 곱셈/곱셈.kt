e421a1f4610460bba92d003958cede82d2b8e521
rarara <155744452+hwoo7622@users.noreply.github.com>
2025-04-05 18:54:32 +0900

    [Bronze III] Title: 곱셈, Time: 76 ms, Memory: 12260 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val A = readLine()!!.toInt()
+    val B = readLine()!!
+    print("${A*Character.getNumericValue(B[2])}\n")
+    print("${A*Character.getNumericValue(B[1])}\n")
+    print("${A*Character.getNumericValue(B[0])}\n")
+    print("${A*B.toInt()}")
+}
+
